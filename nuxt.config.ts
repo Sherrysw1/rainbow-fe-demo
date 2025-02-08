8118f5da71001bc8c49c8a3d996e2d2d04a41d48
sunwenxin08 <sunwenxin08@kuaishou.com>
2025-02-08 15:25:49 +0800

    change
    change

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,11 +1,17 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  extends: 'content-wind',
+
   modules: [
-    '@nuxt/content',
+      '@nuxthq/studio'
   ],
-  devtools: { enabled: true },
-  future: {
-    compatibilityVersion: 3,
+
+  studio: {
+    enabled: true
   },
-  compatibilityDate: '2024-04-03',
+
+  routeRules: {
+    '/': { prerender: true }
+  },
+
+  compatibilityDate: '2024-09-11'
 })
